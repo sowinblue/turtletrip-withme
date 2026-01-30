2d5f9afbee24b2abc6b1c721034077dccdbe8e5c
손예진 <navyartist7@gmail.com>
2026-01-30 13:55:05 +0900

    app.py에 주석 추가, 서버에 사용할 flask 추가, C,R,U,D 기능 정리

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 #         is_completed_count += 1
 
 import json
+import flask
 
 def get_user_progress():
     try:
@@ -20,8 +21,13 @@ def get_user_progress():
         return "파일을 찾을 수 없습니다."
     
 
-# def save_user_progress():
+# def save_user_progress(): # 할일 저장용 함수 -html에서 사용자에게 사용자가 입력한 할일 받아오기
 
+
+# C: 할일 만들기
+# R: 조회하기 (HTML 화면)
+# U: 할일 수정 기능 (버튼 어디에?)
+# D: 할일 삭제 기능 (버튼 어디에?2)
 
 # 실행 및 결과 확인
 result = get_user_progress()
